e1231cf4b248e815a504d1e8a18d38feba70918c
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-07-24 16:24:26 -0300

    header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,20 @@
-import { Link } from 'react-router-dom';
-import styles from './Header.module.css';
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar
+        </Link>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

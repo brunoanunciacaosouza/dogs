85f3d2c59d33f18fc2efa1399facbffbc7d3c159
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-07-24 18:46:26 -0300

    useForm

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Home from './Components/Home';
-import Login from './Login/Login';
+import Login from './Components/Login/Login';
 
 import './App.css';
 
--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,11 +1,21 @@
 import styles from "./Input.module.css";
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange, onBlur, error }) => {
   return (
     <div className={styles.wrapper}>
-      <label className={styles.label} htmlFor={name}>{label}</label>
-      <input id={name} className={styles.input} type={type} name={name} />
-      <p className={styles.error}>error</p>
+      <label className={styles.label} htmlFor={name}>
+        {label}
+      </label>
+      <input
+        id={name}
+        className={styles.input}
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };

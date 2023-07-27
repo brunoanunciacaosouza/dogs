562e8006d167aee895266b86d78873d6583587ab
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-07-27 16:52:19 -0300

    userContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import Home from './Components/Home';
-import Login from './Components/Login/Login';
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 
-import './App.css';
+import { UserStorage } from "./UserContext";
+
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login/*' element={<Login/>}/>
-      </Routes>
-      <Footer/>
+      <UserStorage>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
+        <Footer />
+      </UserStorage>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;

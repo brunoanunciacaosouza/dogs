6cc1df3f8a97fe848eecb4daa7e9b34aa9382e33
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-07-27 21:27:55 -0300

    login background

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -6,20 +6,24 @@ import LoginPasswordLost from "./LoginPasswordLost";
 import { useContext } from "react";
 import { UserContext } from "../../UserContext";
 
+import styles from "./Login.module.css";
+
 const Login = () => {
   const { login } = useContext(UserContext);
 
   if (login === true) return <Navigate to="/conta" />;
 
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="perdeu" element={<LoginPasswordLost />} />
-        <Route path="resetar" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 

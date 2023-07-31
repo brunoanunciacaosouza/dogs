7c8f1b9560e0a8fc3318cedc4b0475303c1623f4
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-07-31 12:22:52 -0300

    UserHeader

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import UserHeader from './UserHeader'
+import { Route, Routes } from 'react-router-dom'
+import Feed from '../Feed/Feed'
+import UserStats from './UserStats'
+import UserPhotoPost from './UserPhotoPost'
 
 const User = () => {
   return (
-    <div>User</div>
+    <section className='container'>
+      <UserHeader/>
+      <Routes>
+        <Route path='/' element={<Feed/>}/>
+        <Route path='/postar' element={<UserPhotoPost/>}/>
+        <Route path='/estatisticas' element={<UserStats/>}/>
+      </Routes>
+    </section>
   )
 }
 
